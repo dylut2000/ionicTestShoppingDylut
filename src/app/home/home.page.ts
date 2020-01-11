4612f52207b94b2ed76da599de074674712e5b62
dylut2000 <hardylutula@gmail.com>
2020-01-11 18:31:21 +0200

    home logic set

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { CartModalPage } from '../pages/cart-modal/cart-modal.page';
+import { BehaviorSubject } from 'rxjs';
+import { CartService } from './../services/cart.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  @ViewChild('cart', { static: false, read: ElementRef }) fab: ElementRef;
 
+  products = [];
+  cart = [];
+  cartItemCount: BehaviorSubject<number>;
+
+  constructor(private cartService: CartService, private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+    console.log(this.products)
+  }
+
+
+  addCart(product) {
+    this.animateCss('tada');
+    this.cartService.addProduct(product);
+  }
+
+  async openCart() {
+
+    this.animateCss('bounceOutLeft', true);
+    let modal = await this.modalCtrl.create({
+      component: CartModalPage,
+      cssClass: 'cart-modal'
+    });
+    modal.onWillDismiss().then(() => {
+      this.fab.nativeElement.classList.remove('animated', 'bounceOutLeft');
+      this.animateCss('bounceInLeft');
+    });
+    modal.present();
+  }
+
+
+  animateCss(animationName, keepAnimated = false) {
+    const node = this.fab.nativeElement;
+    node.classList.add('animated', animationName);
+
+    function handleAnimationEnd() {
+      if (!keepAnimated) {
+        node.classList.remove('animated', animationName);
+      }
+      node.removeEventListener('animationend', handleAnimationEnd);
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+  }
 }
